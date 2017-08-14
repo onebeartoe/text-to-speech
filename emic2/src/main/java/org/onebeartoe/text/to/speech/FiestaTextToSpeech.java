05a05b6eafde4fcdcdb2664cf39de74a1490e9f7
onebeartoe <onebeartoe@gmail.com>
2017-08-14 09:53:39 -0500

    This is the start of a Fiesta implementation of text to speach.

---
+++ emic2/src/main/java/org/onebeartoe/text/to/speech/FiestaTextToSpeech.java
@@ -0,0 +1,60 @@
+
+package org.onebeartoe.text.to.speech;
+
+/**
+ * @author Roberto Marquez <https://www.youtube.com/user/onebeartoe>
+ */
+public class FiestaTextToSpeech implements TextToSpeech
+{
+
+    @Override
+    public void demoMessage(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String currentTime() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void pause() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void resume() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setLanguage(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setSpeakingRate(int rate) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setVoice(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setWordsPerMinute(int rate) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void speak(String text) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void stop() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ emic2/src/main/java/org/onebeartoe/text/to/speech/TextToSpeechImplementations.java
@@ -0,0 +1,11 @@
+
+package org.onebeartoe.text.to.speech;
+
+/**
+ * @author Roberto Marquez <https://www.youtube.com/user/onebeartoe>
+ */
+public enum TextToSpeechImplementations 
+{
+    EMIC2,
+    FESTIVAL
+}
